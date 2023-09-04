0693e35fe3e785271e4b632061a3aaa98c60b5b0
John <jjpyae@gmail.com>
2023-09-03 22:19:20 -0700

    fixed cypress test files in cypress/e2e folder

--- cypress/e2e/test2.cy.js
+++ cypress/e2e/test2.cy.js
@@ -1,20 +1,20 @@
 // Phase 2: Tracking Clicks
 describe("Phase 2: Tracking Clicks", function () {
 
-    beforeEach(function () {
+    before(function () {
         cy.visit("public/index.html");
     });
 
     describe("Placing Symbols", function () {
         it("The first click places an 'X' in that cell", function () {
-            cy.get(document.body).get("#square-0").click().children()
+            cy.get("#square-0").click().children()
                 .should("have.attr", "src", "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg");
         });
 
         it("The next click places an 'O' in that cell", function () {
-            cy.get(document.body).get("#square-0").click().children()
+            cy.get("#square-0").click().children()
                 .should("have.attr", "src", "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg");
-            cy.get(document.body).get("#square-3").click().children()
+            cy.get("#square-3").click().children()
                 .should("have.attr", "src", "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg");
         });
     });
@@ -30,30 +30,31 @@ describe("Phase 2: Tracking Clicks", function () {
         });
 
         it("Clicking a cell that already has a symbol does nothing", function () {
-            cy.get(document.body).get("#square-0").click().children()
+            cy.get("#square-0").click().children()
                 .should("have.attr", "src", "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg");
-
-            cy.get(document.body).get("#square-3").click().children()
-                .should("have.attr", "src", "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg");
-
             cy.get("#square-3").click().children()
                 .should("have.attr", "src", "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg");
-
+            cy.get("#square-3").click().children()
+                .should("have.attr", "src", "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg");
             cy.get("#square-0").click().children()
                 .should("have.attr", "src", "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg");
         });
 
         it("Clicking a cell that already has a symbol does not change next player symbol", function () {
-            cy.get("#square-2").click()
-            cy.get("#square-3").click()
+            cy.get("#square-2").click();
+            cy.get("#square-3").click();
             cy.get("#square-4").click().children()
                 .should("have.attr", "src", "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg");
-            cy.get("#square-4").click()
+            cy.get("#square-4").click();
             cy.get("#square-5").click().children()
                 .should("have.attr", "src", "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg");
         });
 
         it("A cell that has not been clicked remains empty", function () {
+            cy.get("#square-2").click();
+            cy.get("#square-3").click();
+            cy.get("#square-4").click();
+            cy.get("#square-5").click();
             cy.get("#square-1 img")
                 .should("not.exist");
             cy.get("#square-6 img")
